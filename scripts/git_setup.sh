4d5fae980559416b258657b0a2c0ef594ac4c6ad
Ethan Holter <holterethan@gmail.com>
2024-02-20 12:33:20 -0600

    fixed git being a whiny little b*tch

--- scripts/git_setup.sh
+++ scripts/git_setup.sh
@@ -3,17 +3,32 @@
 # idempotent - yes
 
 sudo apt install git
+sudo apt install gpg
 
-read -p "Enter fullname: " FULLNAME
-read -p "Enter email: " EMAIL
+read -p "Configure name and email? (y/n) " A
 
-git config --global --replace-all user.name $FULLNAME
-git config --global --replace-all user.email $EMAIL
+if [ $A = "y" ]; then
+  read -p "Enter fullname: " FULLNAME
+  read -p "Enter email: " EMAIL
+  git config --global --replace-all user.name $FULLNAME
+  git config --global --replace-all user.email $EMAIL
+fi
+unset A
+
+
+
 git config --global --replace-all pull.rebase true
+git config --global --replace-all rebase.autoStash true
+git config --global --replace-all core.editor "nano"
+git config --global --replace-all color.ui true
+git config --global --replace-all credential.credentialStore gpg
+
+# configure git credential manager-core
+
+if ! command -v git-credential-manager; then
+  git config --global --unset credential.helper
+  curl -L https://aka.ms/gcm/linux-install-source.sh | bash
+  git-credential-manager configure
+fi
 
 echo "git setup successfully"
-
-# FOR ONCE IN MY GODDAMN LIFE I WANT GIT TO NOT WORK SO I CAN TEST IT BUT IT WONT FUCKING BREAK
-# FUCKKCKCKCKCKCKCKCCKCKCKCK
-# THIS SHIT HAS NEVER FUCKING WORKED UNTIL NOW WTFFFFF
-
