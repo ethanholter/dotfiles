37411840c9692662d9308489a18a4c70870cdfec
Ethan Holter <holterethan@gmail.com>
2024-02-20 00:12:17 -0600

    more git setup

--- scripts/git_setup.sh
+++ scripts/git_setup.sh
@@ -4,5 +4,9 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root" && exit 1
 fi
 
-sudo -u $SUDO_USER git config --global user.email "holterethan@gmail.com"
-sudo -u $SUDO_USER git config --global user.name "Ethan Holter"
+
+sudo -u $SUDO_USER git config --global --replace-all user.email "holterethan@gmail.com"
+sudo -u $SUDO_USER git config --global --replace-all user.name "Ethan Holter"
+sudo -u $SUDO_USER git config --global --replace-all pull.rebase true
+
+echo "Done"
