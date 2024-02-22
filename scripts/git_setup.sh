7ad489ca3c407c21b2ea017e79bd3bc0769edbc6
Ethan Holter <holterethan@gmail.com>
2024-02-21 21:09:12 -0600

    fixed it

--- scripts/git_setup.sh
+++ scripts/git_setup.sh
@@ -2,15 +2,16 @@
 
 # idempotent - yes
 
-sudo apt install git
-sudo apt install gpg
+yes | sudo apt install git gpg pass
+
+pass init GitHub
 
 read -p "Configure name and email? (y/n) " A
 
 if [ $A = "y" ]; then
   read -p "Enter fullname: " FULLNAME
   read -p "Enter email: " EMAIL
-  git config --global --replace-all user.name $FULLNAME
+  git config --global --replace-all user.name "${FULLNAME}"
   git config --global --replace-all user.email $EMAIL
 fi
 unset A
@@ -19,16 +20,9 @@ unset A
 
 git config --global --replace-all pull.rebase true
 git config --global --replace-all rebase.autoStash true
-git config --global --replace-all core.editor "nano"
+git config --global --replace-all core.editor "code"
 git config --global --replace-all color.ui true
+git config --global --unset credential.helper
 git config --global --replace-all credential.credentialStore gpg
 
-# configure git credential manager-core
-
-if ! command -v git-credential-manager; then
-  git config --global --unset credential.helper
-  curl -L https://aka.ms/gcm/linux-install-source.sh | bash
-  git-credential-manager configure
-fi
-
-echo "git setup successfully"
+echo "git setup completed"
